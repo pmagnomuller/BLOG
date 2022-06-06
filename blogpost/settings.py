4dbde5dd9b45166fb2d90a302961621e043940e7
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2022-06-06 19:20:52 +0200

    add static

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / "static_cdn"
+MEDIA_ROOT = BASE_DIR / "media_cdn"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
